4f1eed594e58100179fd2902155df7197ac298cb
descent <descent0@gmail.com>
2017-05-18 18:56:31 +0800

    add sub_func

--- m.cpp
+++ m.cpp
@@ -162,6 +162,75 @@ int add_func(const Line &l)
   return 0;
 }
 
+int sub_func(const Line &l)
+{
+  int op1_type;
+  int op2_type;
+
+  cout << "handle add" << endl;
+
+  if (l.size() != 3)
+  {
+    cout << "syntax error" << endl;
+  }
+
+  op1_type = check_operand_type(l[1]);
+  op2_type = check_operand_type(l[2]);
+
+  cout << l[1] << " op type: " << op1_type << endl;
+  cout << l[2] << " op type: " << op2_type << endl;
+
+  u8 mod_rm=0xe8;
+  u8 rm;
+  if ((op1_type | op2_type) == 0x3)
+  { // subl $2, %esp
+    cout << "handle r, imm8" << endl;
+    int reg_val = reg_to_val(l[2].substr(1));
+    cout << "  reg_val: " << reg_val << endl;
+    if (4 == reg_val) 
+    {
+      rm = 4;
+    }
+
+    mod_rm |= rm;
+    cout << "\tmod_rm: " << hex << (u32)mod_rm << dec << endl;
+
+    //string imm_str = string{"$0xf"}.substr(1);
+    string imm_str = l[1].substr(1);
+    cout << "imm_str: " << imm_str << endl;
+    u32 imm_num = std::stoul(imm_str, 0, 0);
+    cout << "imm_num: " << imm_num << endl;
+    //u32 imm_num = std::stoul("0xabcdef12", 0, 16);
+
+    u8 op = 0x83;
+    u8 imm_size = 1; // 1 byte
+    if (imm_num <= 0xff)
+    {
+      cout << imm_num << ": imm8" << endl;
+
+    }
+    #if 0
+    else if ((imm_num <=  0xffff) != 0)
+         {
+           cout << imm_num << ": imm16" << endl;
+           imm_size = 2;
+           op = 0x81;
+         }
+    #endif
+         else
+         {
+           cout << imm_num << ": imm32" << endl;
+           imm_size = 4;
+           op = 0x81;
+         }
+
+    fwrite(&op, 1, 1, fs);
+    fwrite(&mod_rm, 1, 1, fs);
+    fwrite(&imm_num, 1, imm_size, fs);
+  }
+  return 0;
+}
+
 // b8 +rd r32, imm32
 int mov_func(const Line &l)
 {
@@ -339,6 +408,7 @@ int main(int argc, char *argv[])
 
   obj_handle.insert({"mov", mov_func});
   obj_handle.insert({"add", add_func});
+  obj_handle.insert({"sub", sub_func});
   obj_handle.insert({"leave", leave_func});
   obj_handle.insert({"push", push_func});
   obj_handle.insert({"ret", ret_func});
